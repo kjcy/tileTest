c9e7e4856a763d274d88554c4a0db710e545c43d
ujuj3602 <ujuj3602@naver.com>
2022-01-11 01:18:16 +0900

    roomtest 씬재작
    roomdata 관련된 test 를 진행

---
+++ tiletest/Assets/script/roomdata.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomdata : MonoBehaviour
+{
+    public enum welldata{none, well, door};
+
+    public welldata Welldata;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
